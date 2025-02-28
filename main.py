56365a4e29aed520394e81ec194f3b4d1a272134
Earnest-Scribbles <jack26blaze@gmail.com>
2025-02-28 23:10:22 +0530

    Add circleshape class

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
